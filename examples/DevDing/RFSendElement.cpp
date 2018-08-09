af51de776aaa73f307cd768443949738b093fdee
Matthias Hertel <mathertel@hotmail.com>
2018-08-09 19:44:06 +0200

    RFSend Element

---
+++ examples/DevDing/RFSendElement.cpp
@@ -0,0 +1,145 @@
+/**
+ * @file RFSendElement.cpp
+ * @brief Element Template class.
+ *
+ * @author Matthias Hertel, https://www.mathertel.de
+ *
+ * @Copyright Copyright (c) by Matthias Hertel, https://www.mathertel.de.
+ *
+ * This work is licensed under a BSD style license.
+ * https://www.mathertel.de/License.aspx.
+ *
+ * More information on https://www.mathertel.de/Arduino
+ *
+ * Changelog:see RFSendElement.h
+ */
+
+
+#include "ElementRegistry.h"
+#include "RFSendElement.h"
+
+// use the intertechno2 code defintions
+#include <intertechno2.h>
+
+#define LOGGER_MODULE "RFSend"
+#include "Logger.h"
+
+/* ===== Define local constants and often used strings ===== */
+
+// like:
+// #define TIMER_UNIT 1000
+
+
+/* ===== Static factory function ===== */
+
+/**
+ * @brief static factory function to create a new RFSendElement
+ * @return RFSendElement* created element
+ */
+Element *RFSendElement::create()
+{
+  LOGGER_INFO("create()");
+  return (new RFSendElement());
+} // create()
+
+
+/* ===== Element functions ===== */
+
+
+// maybe: overwrite the init() function.
+
+
+/**
+ * @brief Set a parameter or property to a new value or start an action.
+ */
+bool RFSendElement::set(const char *name, const char *value)
+{
+  LOGGER_INFO("set(%s, %s)", name, value);
+  bool ret = true;
+
+  if (_stricmp(name, "value") == 0) {
+    _lastValue = atoi(value);
+
+    if (active) {
+      if (_lastValue)
+        tabRF.send(_codeOn.c_str());
+      else
+        tabRF.send(_codeOff.c_str());
+    } // if
+
+  } else if (_stricmp(name, "codeOn") == 0) {
+    _codeOn = value;
+
+  } else if (_stricmp(name, "codeOff") == 0) {
+    _codeOff = value;
+
+  } else {
+    ret = Element::set(name, value);
+  } // if
+
+  return (ret);
+} // set()
+
+
+/**
+ * @brief Activate the RFSendElement.
+ */
+void RFSendElement::start()
+{
+  LOGGER_INFO("start()");
+
+  // Verify parameters
+
+  // if (parameters ok) {
+  Element::start();
+  // } // if
+
+  // initialize the tabRF library
+  tabRF.init(NO_PIN, D4); // input at D1 = pin #2 , output at D4, pin # 9
+  tabRF.setupDefition(&Intertechno2_Sequence);
+  // tabRF.attachCallback(receiveCode);
+
+
+} // start()
+
+
+/**
+ * @brief Give some processing time to the Element to check for next actions.
+ */
+void RFSendElement::loop()
+{
+  // do something
+} // loop()
+
+
+/**
+ * @brief push the current value of all properties to the callback.
+ */
+void RFSendElement::pushState(
+    std::function<void(const char *pName, const char *eValue)> callback)
+{
+  Element::pushState(callback);
+  callback("value", (_lastValue == 1) ? "1" : "0");
+} // pushState()
+
+
+// maybe: overwrite the term() function,
+
+
+/* ===== Register the Element ===== */
+
+// As long as the Element is project specific or is a element always used
+// the registration is placed here without using a register #define.
+
+// When transferred to the HomeDing library a #define like the
+// HOMEDING_INCLUDE_RFSEND should be used to allow the sketch to select the
+// available Elements. See <HomeDing.h> the move these lines to
+// RFSendElement.h
+
+// #ifdef HOMEDING_REGISTER
+// Register the RFSendElement onto the ElementRegistry.
+bool RFSendElement::registered =
+    ElementRegistry::registerElement("rfsend", RFSendElement::create);
+// #endif
+
+// End

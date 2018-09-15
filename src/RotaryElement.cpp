4411ee2380e92b86922849276d0fae7a1e39cc9a
Matthias Hertel <mathertel@hotmail.com>
2018-09-15 17:26:36 +0200

    Rotary Element added

---
+++ src/RotaryElement.cpp
@@ -0,0 +1,132 @@
+/**
+ * @file RotaryElement.cpp
+ * @brief Element Template class.
+ *
+ * @author Matthias Hertel, https://www.mathertel.de
+ *
+ * @Copyright Copyright (c) by Matthias Hertel, https://www.mathertel.de.
+ *
+ * This work is licensed under a BSD style license.
+ * https://www.mathertel.de/License.aspx.
+ *
+ * More information on https://www.mathertel.de/Arduino
+ *
+ * Changelog:see RotaryElement.h
+ */
+
+#include "RotaryElement.h"
+#include "ElementRegistry.h"
+
+#define LOGGER_MODULE "rotary"
+#include "core/Logger.h"
+
+/* ===== statics ===== */
+
+/**
+ * @brief The Instance of the Rotary Encoder library.
+ * Because of the interrupt implementation ther can be only one rotary encoder used at the same time.
+ */
+RotaryEncoder *__encoder;
+
+
+/* ===== Static factory function ===== */
+
+/**
+ * @brief static factory function to create a new RotaryElement
+ * @return RotaryElement* created element
+ */
+Element *RotaryElement::create()
+{
+  LOGGER_TRACE("create()");
+  return (new RotaryElement());
+} // create()
+
+
+/* ===== Element functions ===== */
+
+/**
+ * @brief Set a parameter or property to a new value or start an action.
+ */
+bool RotaryElement::set(const char *name, const char *value)
+{
+  LOGGER_TRACE("set(%s, %s)", name, value);
+  bool ret = true;
+
+  if (_stricmp(name, "value") == 0) {
+    _value = atoi(value);
+
+  } else if (_stricmp(name, "pin1") == 0) {
+    _pin1 = _atopin(value);
+
+  } else if (_stricmp(name, "pin2") == 0) {
+    _pin2 = _atopin(value);
+
+  } else if (_stricmp(name, "onChange") == 0) {
+    _changeAction = value;
+
+  } else {
+    ret = Element::set(name, value);
+  } // if
+
+  return (ret);
+} // set()
+
+
+/**
+ * @brief Check the signals from the rotary encoder 
+ */
+void __checkPosition()
+{
+  __encoder->tick(); // just call tick() to check the state.
+}
+
+/**
+ * @brief Activate the RotaryElement.
+ */
+void RotaryElement::start()
+{
+  LOGGER_TRACE("start()");
+
+  // Verify parameters
+  if ((_pin1 < 0) || (_pin2 < 0)) {
+    LOGGER_ERR("pin values are required.");
+
+  } else {
+    __encoder = new RotaryEncoder(_pin1, _pin2);
+    attachInterrupt(_pin1,  __checkPosition, CHANGE);
+    attachInterrupt(_pin2,  __checkPosition, CHANGE);
+    Element::start();
+  } // if
+} // start()
+
+
+/**
+ * @brief Give some processing time to the Element to check for next actions.
+ */
+void RotaryElement::loop()
+{
+  // do something
+  __encoder->tick(); // just call tick() to check the state.
+  char tmp[10];
+
+  long newPos = __encoder->getPosition();
+  if (newPos != _value) {
+    // send an action with the delta
+    itoa(newPos - _value, tmp, sizeof(tmp));
+    _board->dispatch(_changeAction, tmp);
+  }
+  _value = newPos;
+} // loop()
+
+
+/**
+ * @brief push the current value of all properties to the callback.
+ */
+void RotaryElement::pushState(
+    std::function<void(const char *pName, const char *eValue)> callback)
+{
+  Element::pushState(callback);
+  callback("value", String(_value).c_str());
+} // pushState()
+
+// End
---
+++ src/RotaryElement.h
@@ -0,0 +1,107 @@
+/**
+ * @file RotaryElement.h
+ * @brief Element Template class.
+ * 
+ * @author Matthias Hertel, https://www.mathertel.de
+ *
+ * @Copyright Copyright (c) by Matthias Hertel, https://www.mathertel.de.
+ *
+ * This work is licensed under a BSD style license.
+ * https://www.mathertel.de/License.aspx.
+ * 
+ * More information on https://www.mathertel.de/Arduino
+ * 
+ * Changelog:
+ * * 15.09.2018 created by Matthias Hertel
+ */
+
+#ifndef RotaryElement_H
+#define RotaryElement_H
+
+#include "Board.h"
+#include "Element.h"
+#include <Arduino.h>
+
+#include <RotaryEncoder.h>
+
+/**
+ * @brief RotaryElement implements...
+ * @details
+@verbatim
+
+The RotaryElement can ...
+
+@endverbatim
+ */
+
+class RotaryElement : public Element
+{
+public:
+  /**
+   * @brief Factory function to create a RotaryElement.
+   * @return Element*
+   */
+  static Element *create();
+
+  /**
+   * @brief static variable to ensure registering in static init phase.
+   */
+  static bool registered;
+
+  /**
+   * @brief initialize a new Element.
+   * @param board The board reference.
+   */
+  // virtual void init(Board *board);
+
+  /**
+   * @brief Set a parameter or property to a new value or start an action.
+   * @param name Name of property.
+   * @param value Value of property.
+   * @return true when property could be changed and the corresponding action
+   * could be executed.
+   */
+  virtual bool set(const char *name, const char *value);
+
+  /**
+   * @brief Activate the Element.
+   * @return true when the Element could be activated.
+   * @return false when parameters are not usable.
+   */
+  virtual void start();
+
+  /**
+   * @brief Give some processing time to the timer to check for next action.
+   */
+  virtual void loop();
+
+  /**
+   * @brief push the current value of all properties to the callback.
+   * @param callback callback function that is used for every property.
+   */
+  virtual void pushState(
+      std::function<void(const char *pName, const char *eValue)> callback);
+
+private:
+  /**
+   * @brief The actual value.
+   */
+  long _value = 0;
+
+  int _pin1 = -1;
+  int _pin2 = -1;
+
+  /**
+   * @brief The _changeAction holds the actions that are submitted when the rotary was changed.
+   */
+  String _changeAction;
+};
+
+
+#ifdef HOMEDING_REGISTER
+// Register the RotaryElement onto the ElementRegistry.
+bool RotaryElement::registered =
+    ElementRegistry::registerElement("rotary", RotaryElement::create);
+#endif
+
+#endif

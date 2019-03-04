1579468035082fc9be9bf9cf17351a80bc319dd9
Matthias Hertel <mathertel@hotmail.com>
2019-03-04 21:59:37 +0100

    PMS5003 Sensor

---
+++ examples/DevDing/PMSElement.cpp
@@ -0,0 +1,212 @@
+/**
+ * @file PMSElement.cpp
+ * @brief Element Template class.
+ *
+ * @author Matthias Hertel, https://www.mathertel.de
+ *
+ * @Copyright Copyright (c) by Matthias Hertel, https://www.mathertel.de.
+ *
+ * This work is licensed under a BSD style license.
+ * https://www.mathertel.de/License.aspx.
+ *
+ * More information on https://www.mathertel.de/Arduino
+ *
+ * Changelog:see PMSElement.h
+ */
+
+
+#include "PMSElement.h"
+#include <Board.h>
+#include <ElementRegistry.h>
+
+#include <cstring>
+
+/* ===== Define local constants and often used strings ===== */
+
+/* ===== Define local MACROS ===== */
+
+// get a sensor data from the data buffer
+// starting data[4+5] = PWSDATA(1) as counted in reference manual
+#define PWSDATA(n) ((_data[2 + 2 * n] << 8) + _data[2 + 2 * n + 1])
+
+/* ===== Static factory function ===== */
+
+// SoftwareSerial pmsSerial2(2, 0, false, 256);
+bool isOpen = false;
+
+/**
+ * @brief static factory function to create a new PMSElement
+ * @return PMSElement* created element
+ */
+Element *PMSElement::create()
+{
+  return (new PMSElement());
+} // create()
+
+
+/* ===== Element functions ===== */
+
+// PMSElement::PMSElement() {}
+
+// maybe: overwrite the init() function.
+// void PMSElement::init(Board *board)
+// {
+//   LOGGER_ETRACE("init()");
+//   Element::init(board);
+//   // do something here like initialization
+// } // init()
+
+
+/**
+ * @brief Set a parameter or property to a new value or start an action.
+ */
+bool PMSElement::set(const char *name, const char *value)
+{
+  LOGGER_ETRACE("set(%s, %s)", name, value);
+  bool ret = true;
+
+  if (_stricmp(name, "pinrx") == 0) {
+    _pinrx = _atopin(value);
+
+  } else if (_stricmp(name, "pintx") == 0) {
+    _pintx = _atopin(value);
+
+  } else if (_stricmp(name, "onChange") == 0) {
+    // save the actions
+    _changeAction = value;
+    // } else if (_stricmp(name, "doAction") == 0) {
+    // make something
+
+  } else {
+    ret = Element::set(name, value);
+  } // if
+
+  return (ret);
+} // set()
+
+
+/**
+ * @brief Activate the PMSElement.
+ */
+void PMSElement::start()
+{
+  LOGGER_ETRACE("start(%d, %d)", _pinrx, _pintx);
+
+  // _pmsSerial->begin(9600);
+  // _pmsSerial->
+  isOpen = false;
+
+  Element::start();
+
+} // start()
+
+
+/**
+ * @brief Give some processing time to the Element to check for next actions.
+ */
+void PMSElement::loop()
+{
+  // do something
+
+  //   uint8_t _data[32];
+  // int _datapos;
+
+  //   if (!isOpen)
+  //     _pmsSerial->begin(9600);
+  //     _pmsSerial->stopListening();
+  // isOpen = true;
+
+  if (!isOpen) {
+    if (!_pmsSerial)
+      _pmsSerial = new SoftwareSerial(_pinrx, _pintx, false, 128);
+    _pmsSerial->begin(9600);
+    isOpen = true;
+    _datapos = 0;
+  }
+
+  if (_pmsSerial->overflow()) {
+    Serial.printf("X");
+    _pmsSerial->end();
+    // delete _pmsSerial;
+    // _pmsSerial = nullptr;
+    isOpen = false;
+    return;
+  }
+
+  while (_pmsSerial->available()) {
+    int b = _pmsSerial->read();
+
+    // raw logging
+    // if (b == 0x42)
+    //   Serial.printf("\n");
+    // Serial.printf("%02x", b);
+
+    // collect data to buffer
+    if ((_datapos == 0) && (b == 0x42)) {
+      // possibly new data package
+      _data[_datapos++] = b; // 0x42 (fixed) starts the data sequence
+    } else if ((_datapos == 1) && (b == 0x4d)) {
+      _data[_datapos++] = b; // 0x4d (fixed) starts the data sequence
+    } else if (_datapos > 1) {
+      _data[_datapos++] = b; // data
+
+      if (_datapos == 32) {
+        // all data received
+        _pmsSerial->end();
+        isOpen = false;
+
+        // check checksum
+        int checksum = 0;
+        int n;
+        for (n = 0; n < 32-2; n++) checksum += _data[n];
+
+        if (checksum == PWSDATA(14)) {
+          // valid data
+          _value = PWSDATA(2);
+          _datapos = 0;
+          // LOGGER_EINFO("value = %02x", _value);
+
+          if (_changeAction.length() > 0)
+            _board->dispatch(_changeAction, _value);
+
+          // for (n = 0; n < 32; n++) Serial.printf("%02x", _data[n]);
+          // Serial.printf("\n");
+        } // if
+
+      } // if
+    } // if
+  }
+
+} // loop()
+
+
+/**
+ * @brief push the current value of all properties to the callback.
+ */
+void PMSElement::pushState(
+    std::function<void(const char *pName, const char *eValue)> callback)
+{
+  Element::pushState(callback);
+  callback("value", String(_value).c_str());
+} // pushState()
+
+
+// maybe: overwrite the term() function,
+
+
+/* ===== Register the Element ===== */
+
+// As long as the Element is project specific or is a element always used
+// the registration is placed here without using a register #define.
+
+// When transferred to the HomeDing library a #define like the
+// HOMEDING_INCLUDE_My should be used to allow the sketch to select the
+// available Elements. See <HomeDing.h> the move these lines to PMSElement.h:
+
+// #ifdef HOMEDING_REGISTER
+// Register the PMSElement onto the ElementRegistry.
+bool PMSElement::registered =
+    ElementRegistry::registerElement("pms", PMSElement::create);
+// #endif
+
+// End
---
+++ examples/DevDing/PMSElement.h
@@ -0,0 +1,103 @@
+/**
+ * @file PMSElement.h
+ * @brief Element Template class.
+ * 
+ * @author Matthias Hertel, https://www.mathertel.de
+ *
+ * @Copyright Copyright (c) by Matthias Hertel, https://www.mathertel.de.
+ *
+ * This work is licensed under a BSD style license.
+ * https://www.mathertel.de/License.aspx.
+ * 
+ * More information on https://www.mathertel.de/Arduino
+ * 
+ * Changelog:
+ * * 30.07.2018 created by Matthias Hertel
+ */
+
+#ifndef PMSELEMENT_H
+#define PMSELEMENT_H
+
+#include <Arduino.h>
+#include <Element.h>
+#include <SoftwareSerial.h>
+
+/**
+ * @brief PMSElement implements...
+ * @details
+@verbatim
+
+The PMSElement can ...
+
+@endverbatim
+ */
+
+class PMSElement : public Element
+{
+public:
+  /**
+   * @brief Factory function to create a PMSElement.
+   * @return Element*
+   */
+  static Element *create();
+
+  /**
+   * @brief static variable to ensure registering in static init phase.
+   */
+  static bool registered;
+
+  /**
+   * @brief initialize a new Element.
+   * @param board The board reference.
+   */
+  // virtual void init(Board *board);
+
+  /**
+   * @brief Set a parameter or property to a new value or start an action.
+   * @param name Name of property.
+   * @param value Value of property.
+   * @return true when property could be changed and the corresponding action
+   * could be executed.
+   */
+  virtual bool set(const char *name, const char *value);
+
+  /**
+   * @brief Activate the Element.
+   * @return true when the Element could be activated.
+   * @return false when parameters are not usable.
+   */
+  virtual void start();
+
+  /**
+   * @brief Give some processing time to the timer to check for next action.
+   */
+  virtual void loop();
+
+  /**
+   * @brief push the current value of all properties to the callback.
+   * @param callback callback function that is used for every property.
+   */
+  virtual void pushState(
+      std::function<void(const char *pName, const char *eValue)> callback);
+
+private:
+  /**
+   * @brief The actual value.
+   */
+  int _value;
+
+  int _pinrx = D4;
+  int _pintx = D3;
+
+  uint8_t _data[32];
+  int _datapos;
+
+SoftwareSerial *_pmsSerial;
+
+  /**
+   * @brief The _xAction holds the actions that is submitted when ...
+   */
+  String _changeAction;
+};
+
+#endif

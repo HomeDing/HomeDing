91888b03e2fc1515429c12b95d4f990c7caf58f3
Matthias Hertel <mathertel@hotmail.com>
2019-11-12 18:26:46 +0100

    Standard example sketch added

---
+++ examples/standard/Standard.ino
@@ -0,0 +1,147 @@
+/**
+ * @file Standard.ino
+ * @brief Standar Sketch that uses the HomeDing Library to implement Things attached the
+ * Internet.
+ * @author Matthias Hertel, https://www.mathertel.de
+ *
+ * @Copyright Copyright (c) by Matthias Hertel, https://www.mathertel.de.
+ *
+ * This work is licensed under a BSD style license,
+ * https://www.mathertel.de/License.aspx.
+ *
+ * More information on https://www.mathertel.de/Arduino and https://homeding.github.io/.
+ *
+ * Changelog:
+ * * 12.11.2019 Standard Example created from development sketch.
+ */
+
+#include <Arduino.h>
+#include <Board.h>
+#include <Element.h>
+
+#include <ESP8266WebServer.h>
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+
+#include <FS.h>
+
+#include <BoardServer.h>
+#include <ElementRegistry.h>
+#include <FileServer.h>
+
+#define HOMEDING_REGISTER 1
+
+// Use the Core Elements of the HomeDing Library
+#define HOMEDING_INCLUDE_CORE
+
+// Use some more Elements that need additional libraries
+#define HOMEDING_INCLUDE_DHT
+#define HOMEDING_INCLUDE_BME680
+#define HOMEDING_INCLUDE_DS18B20
+#define HOMEDING_INCLUDE_RFSend
+#define HOMEDING_INCLUDE_ROTARY
+#define HOMEDING_INCLUDE_MENU
+
+#define HOMEDING_INCLUDE_NTPTIME
+#define HOMEDING_INCLUDE_DSTIME
+
+// Use some more Elements for Displays
+#define HOMEDING_INCLUDE_DISPLAY
+#define HOMEDING_INCLUDE_DISPLAYLCD
+#define HOMEDING_INCLUDE_DISPLAYSSD1306
+#define HOMEDING_INCLUDE_DISPLAYSH1106
+
+// #define HOMEDING_INCLUDE_SERIALLINE
+
+#include <HomeDing.h>
+#include <MicroJsonComposer.h>
+
+static const char respond404[] PROGMEM =
+R"==(<html><head><title>File not found</title></head><body>
+File not found</body></html>)==";
+
+// ===== WLAN credentials =====
+
+#include "secrets.h"
+
+// need a WebServer
+ESP8266WebServer server(80);
+
+// ===== application state variables =====
+
+Board mainBoard;
+
+// ===== implement =====
+
+void handleRedirect()
+{
+  LOGGER_RAW("Redirect...");
+
+  String url;
+  if (mainBoard.boardState < BOARDSTATE_STARTCAPTIVE) {
+    url = mainBoard.homepage;
+  } else {
+    url = "http://";
+    url.concat(WiFi.softAPIP().toString()); // mainBoard.deviceName
+    url.concat("/$setup");
+  }
+  server.sendHeader("Location", url, true);
+  server.send(302);
+  server.client().stop();
+} // handleRedirect()
+
+
+/**
+ * Setup all components and Serial debugging helpers
+ */
+void setup(void)
+{
+  Serial.begin(115200);
+
+  Serial.setDebugOutput(false);
+
+  LOGGER_INFO("HomeDing Device is starting...");
+
+  // ----- setup the file system and load configuration -----
+  SPIFFS.begin();
+  mainBoard.init(&server);
+
+  // ----- adding web server handlers -----
+  // redirect to index.htm when only domain name is given.
+  server.on("/", HTTP_GET, handleRedirect);
+
+  // Board status and actions
+  server.addHandler(new BoardHandler(&mainBoard));
+
+  // Static files in the file system.
+  server.addHandler(new FileServerHandler(SPIFFS, "NO-CACHE"));
+
+  server.onNotFound([]() {
+    const char *uri = server.uri().c_str();
+    LOGGER_RAW("notFound: %s", uri);
+
+    if ((mainBoard.boardState == BOARDSTATE_RUNCAPTIVE) &&
+        ((strcmp(uri, "/connecttest.txt") == 0) ||
+         (strcmp(uri, "/redirect") == 0) ||
+         (strcmp(uri, "/generate_204") == 0) ||
+         (strcmp(uri, "/more.txt") == 0))) {
+      handleRedirect();
+    } else {
+      // standard not found in browser.
+      server.send(404, TEXT_HTML, FPSTR(respond404));
+    }
+  });
+
+  LOGGER_INFO("setup done.");
+} // setup
+
+
+// handle all give time to all Elements and active components.
+void loop(void)
+{
+  server.handleClient();
+  mainBoard.loop();
+} // loop()
+
+
+// end.

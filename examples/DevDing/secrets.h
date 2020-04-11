9c87449a79dc41d2fb4b74b05e7f7871f220f039
Matthias Hertel <mathertel@hotmail.com>
2020-04-11 16:40:32 +0200

    doku

--- examples/DevDing/secrets.h
+++ examples/DevDing/secrets.h
@@ -1,5 +1,11 @@
 // Secrets for your local home network
 
+// This is a "hard way" to configure your local WiFi network name and passphrase
+// into the source code and the uploaded sketch.
+// 
+// Using the WiFi Manager is preferred and avoids reprogramming when your network changes.
+// See https://homeding.github.io/#page=/wifimanager.md
+
 // ssid and passPhrase can be used when compiling for a specific environment.
 
 // add you wifi network name and PassPhrase or use WiFi Manager
